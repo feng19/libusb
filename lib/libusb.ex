dba2f2b397460fa33d8ed3cfec56d39541828d94
feng19 <feng_19@foxmail.com>
2022-11-28 16:37:18 +0800

    feat: add funs to LibUSB module

--- lib/libusb.ex
+++ lib/libusb.ex
@@ -1,4 +1,10 @@
 defmodule LibUSB do
   @moduledoc false
+  alias LibUSB.Native
 
+  defdelegate info, to: Native
+  defdelegate list_devices, to: Native
+  defdelegate open(vendor_id, product_id), to: Native
+  defdelegate write_bulk(device_handle, raw, timeout \\ 2), to: Native
+  defdelegate read_bulk(device_handle, timeout \\ 2), to: Native
 end
